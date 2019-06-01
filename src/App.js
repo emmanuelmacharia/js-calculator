d82443431d4f382ff1ad5a8ff8b191d80c2c4508
emmanuelmacharia <samuelmarsha@outlook.com>
2019-06-01 10:28:53 +0300

    completed the footer ui

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import './App.css';
 
+//components
+import Footer from './components/Footer'
+
 function App() {
   return (
     <div className="App">
       Hello, this is my JavaScript Calculator application
+      <Footer />
     </div>
   );
 }
